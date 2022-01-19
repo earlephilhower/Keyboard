235771ab86b50344faea6efa6d902d9e708c6de4
Eric HANACEK <eric.hanacek@laposte.net>
2022-01-19 23:53:47 +0100

    Modification to have the other keyboards
    Modification to have the other keyboards, for me the AZERTY keyboard

--- examples/KeyboardPassword/KeyboardPassword.ino
+++ examples/KeyboardPassword/KeyboardPassword.ino
@@ -5,8 +5,13 @@
 
 void setup() {
   Serial.begin(115200);
-  Keyboard.begin();
-  delay(5000);
+  //Keyboard.begin();
+  Keyboard.begin(KeyboardLayout_fr_FR);
+  //Keyboard.begin(KeyboardLayout_de_DE);
+  //Keyboard.begin(KeyboardLayout_en_US);
+  //Keyboard.begin(KeyboardLayout_es_ES);
+  //Keyboard.begin(KeyboardLayout_it_IT);
+ delay(5000);
   Serial.printf("Arduino USB Password Typer\n");
   Serial.printf("Press BOOTSEL to enter your super-secure(not!) password\n\n");
 }
